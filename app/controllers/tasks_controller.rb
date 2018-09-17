c768d7b086bb5860d7ecb749db1a52e49b179582
Semret Nicodimos <snicodimos@gmail.com>
2018-09-17 14:33:33 -0700

    Complete baseline steps

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
+TASKS = [ { task: "Get laundry", status: "incomplete"},
+  { task: "Do CS Fun HW", status: "incomplete"},
+  { task: "take dog to vet", status: "complete"}]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
 end
