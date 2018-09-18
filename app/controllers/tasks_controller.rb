34baabac54cabbc5ff59115c3d3ef7b29531ab81
Semret Nicodimos <snicodimos@gmail.com>
2018-09-18 11:40:33 -0700

    Created a db of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,12 @@ class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
+  end
 end
---
+++ db/migrate/20180918183529_add_completion_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddCompletionToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :completion_date, :string
+  end
+end
