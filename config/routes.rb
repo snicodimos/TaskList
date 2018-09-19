c3d48a2d79b91301f04b2d846b9fa4556fd62678
Semret Nicodimos <snicodimos@gmail.com>
2018-09-19 12:12:02 -0700

    adding REST links in route, and some css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+# the "as:" tasks part is defining a nickname for a path (in this case'/tasks')
+get '/tasks', to: 'tasks#index'
+# Therefore, I can refer to this path '/tasks' with the nickname formed by the patternb: tasks_path (which is the nickname + _path)
+
+#nickname task_path
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+
 end
