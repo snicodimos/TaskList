ad243f99e99d6f40ebf34e427141ca2b64a1c1b9
Semret Nicodimos <snicodimos@gmail.com>
2018-09-19 17:56:37 -0700

    added the new task adding capabilities

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,24 @@ class TasksController < ApplicationController
       head :not_found
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    )
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+
 end
